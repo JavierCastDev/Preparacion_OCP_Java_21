ead0ce48fa9ebf1f99ebfc221707fb043db72e24
Javier Castillo <javier.developer.kt@gmail.com>
2026-02-04 23:09:01 -0600

    Add: DeclarationAndDefinition

---
+++ src/certificacion/DeclarationAndDefinition.java
@@ -0,0 +1,31 @@
+package certificacion;
+
+//A declaration just means that something exists.
+
+//A definition describes exactly what it is.
+
+//Example:
+
+public class DeclarationAndDefinition { //This is the Class declaration
+
+    //Here the Class definition starts
+
+    public void exampleDefinition(){ //This is the method declaration
+
+        //Here the Method definition starts
+
+    } //Method definition ends
+} //Class definition ends
+
+
+//A declaration provides partial information
+
+//Definition makes it complete and thus, usable
+
+
+
+/*But if we talk about variables, Java doesn't have a distinction between declaration and definition, because
+all the information is included in the declaration itself.
+ */
+
+//int number; -> this declaration-cum-definition is complete in itself
