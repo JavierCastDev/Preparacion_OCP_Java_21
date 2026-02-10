c5c55cdc1b8d4ae3b468a2ac1086d1b917f2f873
Javier Castillo <javier.developer.kt@gmail.com>
2026-02-09 22:15:50 -0600

    Add: HelloWorld

---
+++ src/certificacion/KeyPoints.java
@@ -0,0 +1,10 @@
+package certificacion;
+
+public class KeyPoints {
+
+    //Java is a compilated language
+
+    //Java creates first a file .class
+
+    //Java use the Garbage Collector to manage memory
+}
