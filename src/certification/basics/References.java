e95e778b00ebddc6a043f7934322bdcaabe69065
Javier Castillo <javier.developer.kt@gmail.com>
2026-02-22 10:51:42 -0600

    feat: add References for OCP certification notes

---
+++ src/certification/basics/References.java
@@ -0,0 +1,30 @@
+package certification.basics;
+
+public class References {
+
+    public static void main(String[] args) {
+
+        /*You can have as many references to an object  as you want.
+        When you assign one reference to another, you basically just copy
+        the value contained in one reference into another.
+         */
+
+        // Create a new String object in the Heap memory
+        var stringOne = new String("Hello");
+
+        // Get the unique identity hash code (memory-like reference) for the first object
+        var reference = System.identityHashCode(stringOne);
+
+        // Print the hash code to the console
+        System.out.println(reference);
+
+        // Assign the reference of the first object to a new variable (Shallow Copy)
+        var stringTwo = stringOne;
+
+        // Get the identity hash code of the second variable
+        var referenceTwo = System.identityHashCode(stringTwo);
+
+        // Print the second hash code (it will be identical to the first one)
+        System.out.println(referenceTwo);
+    }
+}
